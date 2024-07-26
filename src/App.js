8c86ffe04faf185b9caaa3a2d0b4601f8c200305
arbabmuhammadramzandev <helloamramzan@gmail.com>
2024-07-26 21:53:58 +0300

    Make dynamic component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import {Switch, Route} from 'react-router-dom';
 import HomepageComponent from "./page/Homepage/HomepageComponent.jsx";
 import ShopPage from './page/ShopPage/ShopPageComponent.jsx';
+import Header from './components/HeaderComponent/HeaderComponent.jsx';
+import SignInComponent from './page/SignIn/SignInComponent.jsx';
 
 function App() {
  
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" exact component={HomepageComponent} />
-        <Route path="/shop" exact component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInComponent} />
     </Switch>
     </div>
   );
